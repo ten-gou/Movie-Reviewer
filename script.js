b131af4a8f9c81e096cf5626eb4fc3e3d150ecec
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-03-14 20:56:34 -0700

    Add files via upload
    Added the index.html and script, so far added skeleton for how the page will look, and enabled the ability to hide the drop-down menus of TV and Movies.

---
+++ script.js
@@ -0,0 +1,19 @@
+var moviesMenu = document.getElementById("dropdown1");
+var tvMenu = document.getElementById("dropdown2");
+function dropMovies(){
+ if (moviesMenu.style.display == 'none'){   
+ moviesMenu.style.display = 'block';
+ }
+ else{
+     moviesMenu.style.display ="none"
+ }
+}
+function dropTV(){
+    if (tvMenu.style.display == 'none'){   
+    tvMenu.style.display = 'block';
+    }
+    else{
+        tvMenu.style.display ="none"
+    }
+   }
+
